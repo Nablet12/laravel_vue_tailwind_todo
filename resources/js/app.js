520a23fdafa53d5ef3cbf9303f47f91750d881dc
Nablet12 <nableta12@gmail.com>
2021-07-04 14:44:41 +0300

    Installation and Initialization for Vue.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+
+import Project from './Components/Project.vue';
+
+new Vue({
+    el: '#app',
+    /*data: {
+        msg: 'Test Message'
+    }*/
+    components: {
+        'project': Project
+    }
+})
